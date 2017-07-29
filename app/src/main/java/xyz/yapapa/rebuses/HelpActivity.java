e15add0755b3b8c04f6268def8144eea19113745
mihanic89 <yakovlev.mihail@gmail.com>
2017-07-29 23:01:40 +0300

    release ver 1.0-1

--- app/src/main/java/xyz/yapapa/rebuses/HelpActivity.java
+++ app/src/main/java/xyz/yapapa/rebuses/HelpActivity.java
@@ -2,6 +2,9 @@ package xyz.yapapa.rebuses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
@@ -10,7 +13,12 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
-        TextView textViewHelp= (TextView) findViewById(R.id.textViewHelp);
-        textViewHelp.setText("111");
+        WebView webViewHelp= (WebView) findViewById(R.id.webViewHelp);
+        WebSettings webSetting = webViewHelp.getSettings();
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setJavaScriptEnabled(true);
+
+        webViewHelp.setWebViewClient(new WebViewClient());
+        webViewHelp.loadUrl("file:///android_asset/help.html");
     }
 }
